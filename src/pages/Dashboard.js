a2f43904217551743f0323a44bf411535a146a16
Meru45 <87946072+Meru45@users.noreply.github.com>
2024-03-03 09:34:31 +0530

    Minor bug fixed in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -46,7 +46,7 @@ const Dashboard = () => {
                 headers: {
                     accept: "application/json",
                     "Accept-Language": "en-US,en;q=0.8",
-                    "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+                    "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
                 },
             });
 
